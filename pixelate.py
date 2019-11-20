453440a1f0369a47cbfebb4d4c63239a78d2d6c0
anthony-chang <54450499+anthony-chang@users.noreply.github.com>
2019-11-20 00:04:43 -0500

    change pixelate method

--- pixelate.py
+++ pixelate.py
@@ -16,29 +16,28 @@ result = np.zeros(shape=(ROWS, COLS, 3), dtype=np.uint8)
 cv2.namedWindow("Result", cv2.WINDOW_NORMAL)
 cv2.moveWindow("Result", width, 0)
 
+
 def magn(r, g, b):
-    return math.sqrt(r*r + g*g + b*b)
+    return math.sqrt(r * r + g * g + b * b)
+
 
 area = 0
 for r in range(ROWS):
     for c in range(COLS):
-        for i in range(int(height/ROWS)):
-            for j in range(int(width/COLS)):
-                avg[r][c] += ogimage[i + (r*int(height/ROWS))][j + (c*int(width/COLS))]
-                area+=1
-        avg[r][c] = avg[r][c]/area #divide by area of sub-rectangle
+        for i in range(int(height / ROWS)):
+            for j in range(int(width / COLS)):
+                avg[r][c] += ogimage[i + (r * int(height / ROWS))][j + (c * int(width / COLS))]
+                area += 1
+        avg[r][c] = avg[r][c] / area  # divide by area of sub-rectangle
         area = 0
         for i in range(3):
-            avg[r][c][i] = min(avg[r][c][i], 255) #make sure nothing is somehow over 255
-        result[r][c][0] = magn(avg[r][c][0], avg[r][c][1], avg[r][c][2])
-        result[r][c][1] = result[r][c][0]
-        result[r][c][2] = result[r][c][0]
+            avg[r][c][i] = min(avg[r][c][i], 255)  # make sure nothing is somehow over 255
+            result[r][c][i] = avg[r][c][i]
 
 while 1:
     cv2.imshow("Original Image", ogimage)
     cv2.imshow("Result", result)
-    if cv2.waitKey(10) & 0xFF ==27:
+    if cv2.waitKey(10) & 0xFF == 27:
         break
-
 
 cv2.destroyAllWindows()
