0d769af0fd7ecf7ef0477f6d98e78ff4d40a0b9a
anthony-chang <54450499+anthony-chang@users.noreply.github.com>
2019-11-20 20:53:30 -0500

    output in RYB food colouring drops

--- pixelate.py
+++ pixelate.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import math
 
 ROWS = 30
 COLS = 24
@@ -17,8 +16,56 @@ cv2.namedWindow("Result", cv2.WINDOW_NORMAL)
 cv2.moveWindow("Result", width, 0)
 
 
-def magn(r, g, b):
-    return math.sqrt(r * r + g * g + b * b)
+def uint8todeg(num):
+    return num / 255 * 359
+
+def brown():
+    return 2, 2, 2
+
+
+def pastel(h):
+    if h >= 291 or h <= 40:
+        return 1, 0, 0
+    if 41 <= h <= 90:
+        return 0, 1, 0
+    if 91 <= h <= 290:
+        return 0, 0, 1
+    return 0, 0, 0
+
+def brightColour(h):
+    if h >= 310 or h <= 25:
+        return 3, 0, 0
+    if 26 <= h <= 40:
+        return 2, 1, 0
+    if 41 <= h <= 50:
+        return 1, 2, 0
+    if 51 <= h <= 70:
+        return 0, 3, 0
+    if 71 <= h <= 150:
+        return 0, 2, 1
+    if 151 <= h <= 190:
+        return 0, 1, 2
+    if 191 <= h <= 270:
+        return 0, 0, 3
+    if 271 <= h <= 295:
+        return 1, 0, 2
+    if 296 <= h <= 309:
+        return 2, 0, 1
+    return 0, 0, 0
+
+def getRYB(h, s, v):
+    h = uint8todeg(h)
+    s = uint8todeg(s)
+    v = uint8todeg(v)
+    if v < 30:
+        return brown()
+    elif s < 30:
+        if v > 65:
+            return pastel(h)
+        else:
+            return brown()
+    else:
+        return brightColour(h)
 
 
 area = 0
@@ -34,10 +81,20 @@ for r in range(ROWS):
             avg[r][c][i] = min(avg[r][c][i], 255)  # make sure nothing is somehow over 255
             result[r][c][i] = avg[r][c][i]
 
+result = cv2.cvtColor(result, cv2.COLOR_BGR2HSV)
+
+out = open("out.txt", "w")
+for i in range(ROWS):
+    for j in range(COLS):
+        r, y, b = getRYB(result[i][j][0], result[i][j][1], result[i][j][2])
+        out.write(str(r) + " " + str(y) + " " + str(b) + ", ")
+    out.write('\n')
+
 while 1:
     cv2.imshow("Original Image", ogimage)
     cv2.imshow("Result", result)
     if cv2.waitKey(10) & 0xFF == 27:
         break
 
+out.close()
 cv2.destroyAllWindows()
